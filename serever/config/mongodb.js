e40693ca031a638b40eea25ba8e5527b8f85aa40
tharindu <202117299+janishapj12@users.noreply.github.com>
2025-06-16 22:15:41 +0530

    new

--- serever/config/mongodb.js
+++ serever/config/mongodb.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const MONGODB_URI = "mongodb+srv://janisha:1812@usermanagement.fajws.mongodb.net/?retryWrites=true&w=majority&appName=usermanagement" ;
+const MONGODB_URI = "mongodb+srv://pujanijani:1812@usermanagement.krolwlk.mongodb.net/?retryWrites=true&w=majority&appName=usermanagement" ;
 const connectdb = async () =>{
     await mongoose.connect(MONGODB_URI);
     console.log("db conected")
